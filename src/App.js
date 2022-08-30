f0908c3467bbc2e5746039c2c88f2a00fad2a98e
sundarcse2k11 <112463937+sundarcse2k11@users.noreply.github.com>
2022-08-30 14:58:20 +0530

    Basic Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-import './App.css'
-import Quiz from './Quiz'
+import './App.css' 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Quiz/>
+      <div>Sundar
       </div>
     )
   }
